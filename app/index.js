02e74d605983def4ba3a1ea5008be5f429500dfe
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2023-12-08 20:16:15 +0530

    installed react and reactdom from npm

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
-const heading = React.createElement("h1", {}, "hello!");
+import React from "react";
+import ReactDOM from "reactDOM";
 
+const heading = React.createElement("h1", {}, "hello!");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
